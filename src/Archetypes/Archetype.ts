a178129baf08d974a3b3bd1ac2465a3ab8cab72e
mbrennerr <marciobrennerbusiness@gmail.com>
2022-04-19 23:44:19 -0300

    ADD: [Archetypes] Class_abstract_Archetype

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,39 @@
+import { EnergyType } from '../Energy';
+
+export default abstract class Archetype {
+  private readonly _name: string;
+  private _special: number;
+  private _cost: number;
+  
+  constructor(name:string) {
+    this._name = name;
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  private static createdArchetypeInstances():number {
+    throw new Error('Not implemented');
+  } 
+
+  abstract get energyType():EnergyType;
+
+  public get name(): string {
+    return this._name;
+  }
+    
+  public get special(): number {
+    return this._special;
+  }
+
+  public set special(value: number) {
+    this._special = value;
+  }
+  
+  public get cost(): number {
+    return this._cost;
+  }
+
+  public set cost(value: number) {
+    this._cost = value;
+  }
+} 
