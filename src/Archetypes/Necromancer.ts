18f287b8c2988b3470c9caea60c2c0ecfb7664e2
mbrennerr <marciobrennerbusiness@gmail.com>
2022-04-20 01:07:58 -0300

    Add:Class_Necromancer

---
+++ src/Archetypes/Necromancer.ts
@@ -0,0 +1,29 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Necromancer extends Archetype {
+  private _energyType: EnergyType;
+  private static _instances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'mana';
+    Necromancer.addInstance();
+  }
+
+  private static addInstance() {
+    this._instances += 1;
+  }
+
+  public static createdArchetypeInstances() {
+    return this._instances;
+  }
+
+  public get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  public set energyType(value: EnergyType) {
+    this._energyType = value;
+  }
+}
